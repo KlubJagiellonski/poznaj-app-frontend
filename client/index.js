273a17b35765d60e2cb68cac7e76709c46e6ba26
radekgronski <gronski.radek@gmail.com>
2017-10-04 01:18:53 +0200

    Added main CSS file and background image.

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 import 'semantic-ui-css/semantic.min.css';
+import './main.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
