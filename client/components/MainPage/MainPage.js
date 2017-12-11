74b6f6b8b6f43828fbe7e1020a8a120142a5356e
radekgronski <gronski.radek@gmail.com>
2017-12-11 21:35:20 +0100

    Header styled with flexbox.

--- client/components/MainPage/MainPage.js
+++ client/components/MainPage/MainPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Header from './Header';
+import Header from '../Header';
 import Content from './Content';
 import Footer from './Footer';
 
--- client/index.js
+++ client/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+// Arkusz stylów wymaganay do poprawnego działania Semantic UI:
 import 'semantic-ui-css/semantic.min.css';
+
 import './main.less';
 
 
