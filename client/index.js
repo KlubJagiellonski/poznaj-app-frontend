9d34aeee966da8c96fa19523d56592ffe248e79a
Radosław Groński <radek.gronski@vazco.eu>
2018-03-24 20:42:02 +0100

    Added web fonts loader & default font - Lato.

--- client/index.js
+++ client/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import WebFont from 'webfontloader';
 
 import App from './components/App';
 
 import './main.less';
+
+
+WebFont.load({
+    google: {
+        families: ['Lato']
+    }
+});
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
